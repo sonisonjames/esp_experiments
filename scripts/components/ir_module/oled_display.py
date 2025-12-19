ba7f8ed5c494933032187d0bb53c9163df53062e
sonisonjames <sonison.james@gmail.com>
2025-12-19 10:40:06 +0800

    Display text in big font and fast scrolling

--- scripts/components/ir_module/oled_display.py
+++ scripts/components/ir_module/oled_display.py
@@ -8,19 +8,34 @@ import time
 # I2C pins for typical ESP32
 i2c = I2C(0, scl=Pin(22), sda=Pin(23))
 oled = ssd1306.SSD1306_I2C(128, 64, i2c)
-scale = 2
+scale = 1
 speed = 0
-direction = "STOP"
+direction = 0
 
+"""
+direction axis:
+                       ^
+                       |
+                       0 degrees
+                       | 
+                       |
+<-- (-90) degrees -- Origin -- (+90) degrees -->
+                       |
+                       |
+                       +180/-180 degrees
+                       |
+                       v
+"""
 def update_display(direction, speed, scale=3):
     oled.fill(0)
-    centered_big_text(oled, "SPEED:", scale)
-    speed_str = "{}".format(abs(speed))
-    centered_big_text(oled, speed_str, scale)
-    #centered_big_text(oled, direction, scale)
+    speed_str = "SPEED:{}".format(abs(speed))
+    centered_big_text(oled, speed_str, scale, 0)
+    dir_str = "DIR:{}".format(direction)
+    centered_big_text(oled, dir_str, scale, 9)
     oled.show()
 
 while True:
+    update_display(direction, speed, scale)
     trans = capture_transitions()
     code = decode_nec(trans)
     update_display(direction, speed, scale)
@@ -49,16 +64,35 @@ while True:
             speed = 90
         elif code == 0xFF9867:
             speed = 100
-        elif code == 0xFF38C7:
-            direction = "STOP"
-        elif code == 0xFF18E7:
-            speed = speed + 10
+        elif code == 0xFF38C7: # OK/STOP
+            direction = 0
+            speed = 0
+        elif code == 0xFFB04F:  # HASH/Accelerate
+            if speed < 100:
+                speed = speed + 10
+        elif code == 0xFF6897:  # STAR/Decelerate
             if speed > 0:
-                direction = "FWD"
-        elif code == 0xFF4AB5:
-            speed = speed - 10
-            if speed < 0:
-                direction = "REV"
+                speed = speed - 10
+        elif code == 0xFF18E7: # UP
+            if direction > 0:
+                direction = direction - 10
+            elif direction < 0:
+                direction = direction + 10
+        elif code == 0xFF4AB5: # DOWN
+            if 0 <= direction < 180:
+                direction = direction + 10
+            elif -180 < direction < 0:
+                direction = direction - 10
+        elif code == 0xFF10EF: # LEFT
+            if -180 <= direction < 90:
+                direction = direction + 10
+            elif 180 <= direction < 90:
+                direction = direction - 10
+        elif code == 0xFF5AA5: # RIGHT
+            if 180 >= direction > -90:
+                direction = direction - 10
+            elif -180 <= direction < -90:
+                direction = direction + 10
     elif code == 0xFFFFFFFF:
         # NEC repeat code (indicates the previous key is being held)
         print("(repeat)")
